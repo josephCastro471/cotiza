aa3a759f47934bd3b0efa2a581b4c87f3cb3cfc6
josephCastro471 <joseph.castror@ug.edu.ec>
2026-07-26 12:53:04 -0500

    fix(web): respect explicit Authorization header in request interceptor

--- src/api/client.js
+++ src/api/client.js
@@ -23,7 +23,7 @@ const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 api.interceptors.request.use((config) => {
   const { token } = leerSesion();
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token && !config.headers.Authorization) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
